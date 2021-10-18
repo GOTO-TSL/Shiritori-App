3709dfd38f4c707268a9a9aa836cfc80f519b329
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-10-18 22:41:20 +0900

    #5 単語詳細画面の遷移処理完了

--- Shiritori/ViewControllers/WordDetailViewController.swift
+++ Shiritori/ViewControllers/WordDetailViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class WordDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    var wordDetailView: WordDetailView!
+    var backButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        // headerに枠線を追加
+        wordDetailView.headerView.addBorder(width: 1.0, color: .white, position: .bottom)
+        wordDetailView.headerView.addBorder(width: 1.0, color: .white, position: .left)
+        wordDetailView.headerView.addBorder(width: 1.0, color: .white, position: .right)
     }
-    */
-
+    
+    private func configureUI() {
+        wordDetailView = WordDetailView()
+        backButton = wordDetailView.headerView.backButton
+        
+        // 配置＆制約の追加
+        view.addSubview(wordDetailView)
+        wordDetailView.addConstraintsToFillView(view)
+        
+        // ボタンにアクションを追加
+        backButton.addTarget(self, action: #selector(backPressed(_ :)), for: .touchUpInside)
+    }
+    
+    @objc private func backPressed(_ sender: UIButton) {
+        addTransition(duration: 0.2, type: .push, subType: .fromLeft)
+        dismiss(animated: false, completion: nil)
+    }
+    
 }
