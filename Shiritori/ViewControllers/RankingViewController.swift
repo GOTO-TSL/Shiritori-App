c82e061dbe1f45aae8d5aca5fa8addf814f5103d
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-10-18 22:22:10 +0900

    #5 ランキング画面の遷移処理完了

--- Shiritori/ViewControllers/RankingViewController.swift
+++ Shiritori/ViewControllers/RankingViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class RankingViewController: UIViewController {
+    
+    // MARK: - Properties
+    var rankingView: RankingView!
+    var backButton: UIButton!
+    
+    // MARK: - Lifecycle
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        // headerに枠線を追加
+        rankingView.headerView.addBorder(width: 1.0, color: .white, position: .bottom)
+        rankingView.headerView.addBorder(width: 1.0, color: .white, position: .left)
+        rankingView.headerView.addBorder(width: 1.0, color: .white, position: .right)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUI() {
+        rankingView = RankingView()
+        backButton = rankingView.headerView.backButton
+        
+        // 配置＆制約の追加
+        view.addSubview(rankingView)
+        rankingView.addConstraintsToFillView(view)
+        
+        // ボタンにアクションを追加
+        backButton.addTarget(self, action: #selector(backPressed(_ :)), for: .touchUpInside)
     }
-    */
-
+    
+    @objc private func backPressed(_ sender: UIButton) {
+        addTransition(duration: 0.3, type: .fade, subType: .fromLeft)
+        dismiss(animated: false, completion: nil)
+    }
 }
